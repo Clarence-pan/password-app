a58eab050be6234aaa51dc23730cb75e9e44d819
Clarence <clarence-pan@foxmail.com>
2016-09-13 13:12:53 +0800

    fix bugs, pack android version

--- js/passwd.js
+++ js/passwd.js
@@ -1,6 +1,10 @@
 function plusReady(){
-    load();
-    bindEvents();
+	try{
+	    load();
+	    bindEvents();		
+	} catch (e){
+		$('body').append("<p>初始化失败：" + e + "</p>");
+	}
 
     function bindEvents() {
         $('#generateBtn').click(function () {
@@ -33,25 +37,35 @@ function plusReady(){
         $('.help-trigger').on('click', function () {
             $('#help').show()[0].scrollIntoView();
         });
+        
+//      if (window.plus){
+//      	$('body').append("<p>你在用html5+ OS: "+plus.os.name+"</p>");
+//      } else {
+//      	$('body').append("<p>你没有在用html5+</p>");
+//      }
 
-		if (window.plus && plus.os.name == "iOS"){
+		if (window.plus && plus.os && plus.os.name == "iOS"){
 			$('#copyBtn').on('click', function(){
 				try{
 					var UIPasteboard = plus.ios.importClass("UIPasteboard");
 					var pasteboard = UIPasteboard.generalPasteboard();
 					pasteboard.setValueforPasteboardType($("#passwd").val() + "", "public.utf8-plain-text");
-					plus.nativeUI.alert("复制成功了！你可以切换到其他应用去粘贴密码啦~");
+					plus.nativeUI.toast("复制成功了！你可以切换到其他应用去粘贴密码啦~");
 				} catch (e){
 					alert("出错了，不能复制：" + e);
 				}
 			});
-		} else if (window.plus && plus.os.name == "iOS") {
+		} else if (window.plus && plus.android) {
 			$('#copyBtn').on('click', function(){
-				var Context = plus.android.importClass("android.content.Context");
-				var main = plus.android.runtimeMainActivity();
-				var clip = main.getSystemService(Context.CLIPBOARD_SERVICE);
-				plush.android.invoke(clip, "setText", $('#passwd').val() + '');
-				plus.nativeUI.alert("复制成功了！你可以切换到其他应用去粘贴密码啦~");
+				try{
+					var Context = plus.android.importClass("android.content.Context");
+					var main = plus.android.runtimeMainActivity();
+					var clip = main.getSystemService(Context.CLIPBOARD_SERVICE);
+					plus.android.invoke(clip, "setText", $('#passwd').val() + '');
+					plus.nativeUI.toast("复制成功了！你可以切换到其他应用去粘贴密码啦~");
+				} catch (e){
+					alert("出错了，不能复制：" + e);
+				}
 			});
 		} else {
 	        var $copyBtn = $('#copyBtn');
